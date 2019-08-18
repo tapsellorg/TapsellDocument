4bbf1c4215e15a352b344bd30c032ba11243c24b
Vahid Mohammadi <vahidm1467@gmail.com>
2019-08-18 11:32:36 +0430

    remove jquery cookie

--- src/_js/bundle.js
+++ src/_js/bundle.js
@@ -1,7 +1,6 @@
 import 'jquery';
 import 'bootstrap';
 // import './old-should-remove/dist/app.min';
-// import './old-should-remove/jquery.cookie.min';
 import './old-should-remove/main';
 import './old-should-remove/nav';
 import './old-should-remove/sidebar';
